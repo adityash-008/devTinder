174ae4fdfcb81e4626f95a5872573af5fe152a07
Aditya Sharma <adityasharma23271@gmail.com>
2025-10-16 00:20:25 +0530

    explored about GET,POST,DELETE calls

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ const express = require('express')
 
 const app = express()
 
-app.use("/test",(req,res) => {   //Request Handler
-    res.send("Hello from the test route!")
+//This will only match the GET call to the "/user"
+app.get(`/user`,(req,res) => {
+    res.send({name:"Aditya Sharma",age:21})
 })
 
-app.use("/",(req,res) => {   
-    res.send("Hello from the Dashboard")
+app.post('/user',(req,res) => {
+    //Saving data to the database
+    res.send("Data Saved Successfully...")
 })
+
+app.delete('/user',(req,res)=> {
+    res.send("Deleted Successfully")
+})
+
+//This will match all the http method API calls to the "/test" route
 app.use("/test",(req,res) => {   
-    res.send("Hello from the test route!")
-})
-
-app.use("/hello",(req,res) => {   
-    res.send("Hello from the hello route!")
+    res.send("This is the test route")
 })
 
 app.listen(3000,()=>{
