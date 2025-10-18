f1041bd2d8f31aaa605c0515a6e6d9ac69ba19e1
Aditya Sharma <adityasharma23271@gmail.com>
2025-10-18 20:14:57 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,24 +2,22 @@ const express = require('express')
 
 const app = express()
 
-//This will only match the GET call to the "/user"
-app.get(`/user`,(req,res) => {
-    res.send({name:"Aditya Sharma",age:21})
-})
-
-app.post('/user',(req,res) => {
-    //Saving data to the database
-    res.send("Data Saved Successfully...")
-})
-
-app.delete('/user',(req,res)=> {
-    res.send("Deleted Successfully")
-})
-
-//This will match all the http method API calls to the "/test" route
-app.use("/test",(req,res) => {   
-    res.send("This is the test route")
-})
+app.use('/user',
+    (req,res,next) => {
+    // res.send("Hello ji ")
+    console.log("Route Handler 1")
+    next()
+},
+ (req,res,next) =>{
+    // res.send("Hello ji 2");
+    console.log("Route Handler 2")
+    next();
+ }
+ ,(req,res,next) =>{
+    // res.send("Hello ji 3");
+    next();
+    console.log("Route Handler 3")
+ });
 
 app.listen(3000,()=>{
     console.log("Server is listening on Port 3000... ")
